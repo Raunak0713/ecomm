3ec65bf7bdca4b4b47f8bc528130ac98c489701f
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-09-30 21:19:51 +0530

    #8 Implemented Products Table

--- app/components/dashboard/DashboardNavigation.tsx
+++ app/components/dashboard/DashboardNavigation.tsx
@@ -1,4 +1,7 @@
+'use client'
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const links = [
@@ -10,10 +13,11 @@ const links = [
 
 // Add className prop along with onItemClick
 const DashboardNavigation = ({ onItemClick, className }: { onItemClick?: () => void, className?: string }) => {
+  const pathname = usePathname()
   return (
     <nav className={className ? className : 'flex gap-4'}>
       {links.map((link) => (
-        <Link key={link.href} href={link.href} onClick={onItemClick}>
+        <Link key={link.href} href={link.href} onClick={onItemClick} className={cn(link.href === pathname ? 'text-black' : 'text-muted-foreground hover:text-foreground')}>
           {link.name}
         </Link>
       ))}
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button'
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { PlusCircle } from 'lucide-react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { MoreHorizontal, PlusCircle, UserIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -22,6 +24,44 @@ const ProductsPage = () => {
             Manage your products and view their sales performance
           </CardDescription>
         </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Image</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead className='text-end'>Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell><UserIcon className='h-16 w-16'/></TableCell>
+                <TableCell>Nike Air</TableCell>
+                <TableCell>Active</TableCell>
+                <TableCell>$299.00</TableCell>
+                <TableCell>2024-06-15</TableCell>
+                <TableCell className='text-end'>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button size={'icon'} variant={'ghost'}>
+                        <MoreHorizontal className='h-4 w-4'/>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align='end'>
+                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </CardContent>
       </Card>
     </>
   )
